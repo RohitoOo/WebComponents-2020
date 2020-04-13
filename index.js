f0fb4b48c5c946ccda7bf313a0ecca82a3a37a10
Rohito Bhambhani <bhambhani.ba@gmail.com>
2020-04-12 20:49:02 -0400

    Added Functionality

--- index.js
+++ index.js
@@ -52,8 +52,10 @@ template.innerHTML = `
     
 
     <h3></h3>
+    <div class='info'>
     <p><slot name='email'/></p>
     <p><slot name='phone'/></p>
+    </div>
     <button id="toggleInfo"> Hide Info </button>
 
       <div>
@@ -64,6 +66,9 @@ template.innerHTML = `
 class userCard extends HTMLElement {
   constructor() {
     super();
+
+
+    this.showInfo = true
 
     // SHADOW DOM
     this.attachShadow({ mode: "open" });
@@ -77,10 +82,33 @@ class userCard extends HTMLElement {
     // `
   }
 
+toggleInfo() {
+   this.showInfo = !this.showInfo
+
+   const info = this.shadowRoot.querySelector('.info')
+   const toggleBtn = this.shadowRoot.querySelector('#toggleInfo')
+
+
+   if(this.showInfo){
+     
+  info.style.display = 'block'
+  toggleBtn.innerText = 'Hide Info'
+
+   }else {
+    info.style.display = 'none'
+    toggleBtn.innerText = 'Show Info'
+  
+   }
+}
+
   connectedCallback(){
       this.shadowRoot.querySelector('#toggleInfo').addEventListener('click', () => 
-          console.log('click click')
+         this.toggleInfo()
       )
+  }
+
+  disconnectedCallback(){
+    this.shadowRoot.querySelector('#toggleInfo').removeEventListener()
   }
 }
 window.customElements.define("user-card", userCard);
